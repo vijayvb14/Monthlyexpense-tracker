f690c8e76d7b99e135a802b88ea4b9cf481d4249
vijayvb14 <37097690+vijayvb14@users.noreply.github.com>
2025-12-30 18:49:14 +0530

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("expense-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
